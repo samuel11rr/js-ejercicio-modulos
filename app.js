2cc345ea4eab6f5e08685d97065e09306142f3ca
Samuel Ramirez <samuel_ramirez@live.com.mx>
2020-01-27 10:41:56 -0600

    ejercicio completo

---
+++ app.js
@@ -0,0 +1,29 @@
+import { getPokemon, renderPokemon, draw as pokemonDraw } from './pokemon.js';
+
+/* Los siguientes dos imports pueden ser cargados desde HTML sin problema */
+import './custom-properties.js';
+import './paint-modules.js'; 
+ 
+
+form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit( event ) {
+    event.preventDefault();
+
+    const data          = new FormData( form ); 
+    const id            = data.get('id');
+    const pokemon       = await getPokemon( id );
+    const pokemonDrawed = await renderPokemon( pokemon );
+    const colors        = pokemonDrawed.colorPalette( 90 );
+    // const colors        = pokemonDraw.colorPalette( 90 );
+    // ambas opciones son posibles
+
+    updateProperties( colors );
+}
+
+
+function updateProperties( colors ) {
+    document.body.style.setProperty( '--primary', `rgb( ${ colors[0].red }, ${ colors[0].green}, ${ colors[0].blue } )` );
+    document.body.style.setProperty( '--secondary', `rgb( ${ colors[1].red }, ${ colors[1].green}, ${ colors[1].blue } )` );
+    document.body.style.setProperty( '--tertiary', `rgb( ${ colors[2].red }, ${ colors[2].green}, ${ colors[2].blue } )` );
+}
